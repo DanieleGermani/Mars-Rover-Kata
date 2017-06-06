2375577409fb79944be25bdbe9d80c17726397a7
Daniele <daniele.germani.85@gmail.com>
2017-06-06 13:56:10 +0200

    ya se mueve

--- rover.js
+++ rover.js
@@ -1,31 +1,14 @@
-var DIRECTIONS = {
-  'N': '-north',
-  'E': '-east',
-  'S': '-south',
-  'W': '-west'
-};
+
 var Rover = {
   position: [0,0],
   direction: 'N',
 };
 
-var COMMAND = 'ffrfrfffrfllffffrlllfflllfff';
+var COMMAND = 'ffrfrfffrbbbblfffffrbbbbblrbbblffff';
 var GRID = {
   rows: 10,
   columns: 10
 };
-/* GRID */
-function Grid() {
-
-
-  for (var i = 0; i < GRID.rows; i++) {
-    for (var j = 0; j < GRID.columns; j++) {
-
-    }
-  }
-}
-
-
 
 /* ROVER */
 // Move
@@ -49,9 +32,9 @@ function move(direction){
       if(roverDirection === "N") {
         roverPosition[1] = (roverPosition[1] === 0) ? 0 : roverPosition[1] + 1;
       } else if (roverDirection === 'E') {
-        roverPosition[0] = (roverPosition[0] === GRID.rows + 1) ? GRID.rows + 1 : roverPosition[0] - 1;
+        roverPosition[0] = (roverPosition[0] === GRID.rows - 1) ? GRID.rows - 1 : roverPosition[0] - 1;
       } else if (roverDirection === 'S') {
-        roverPosition[1] = (roverPosition[1] === GRID.columns + 1) ? GRID.columns + 1 : roverPosition[1] - 1;
+        roverPosition[1] = (roverPosition[1] === GRID.columns - 1) ? GRID.columns - 1 : roverPosition[1] - 1;
       } else if (roverDirection === 'W') {
         roverPosition[0] = (roverPosition[0] === 0) ? 0 : roverPosition[0] + 1;
       }
@@ -109,18 +92,18 @@ function execCommand(command) {
     var letter = letters[i];
     if (letter === 'f' || letter === 'b') {
        move(letter);
-
+console.log(' Direction: ' +Rover.direction+ ' New Rover position: ['+ Rover.position[0] + ', ' + Rover.position[1] + ']')
     }
 
     if (letter === 'r' || letter === 'l') {
        turn(letter);
-console.log('New  position: ['+ Rover.position[0] + ', ' + Rover.position[1] + ']')
+
     }
   }
 }
 
 /* INIT */
-
 moveRover(Rover.position);
 turnRover(Rover.direction)
+
 execCommand(COMMAND);
